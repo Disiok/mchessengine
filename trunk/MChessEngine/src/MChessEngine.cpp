a5fc1d6d4885e5b131c61f0b504848253fbee4f3
microbot01@gmail.com <microbot01@gmail.com@91017150-5900-139c-9019-248a9390ac6b>
2012-07-18 21:35:44 +0000

    makeMove is done I think. Assumes all moves are legal (which makes testing a lot easier as you can see in int main() )

--- trunk/MChessEngine/src/MChessEngine.cpp
+++ trunk/MChessEngine/src/MChessEngine.cpp
@@ -28,17 +28,53 @@ int main() {
 	p.make_move(0x3424);//nonexistent
 	p.make_move(0x5434);//flying pawn capture time
 	p.make_move(0x3375);//move out black bishop
-	p.make_move(0x5554); //some nonsense moves while I move out black's kingside pieces
+	//some nonsense moves while I move out black's kingside pieces
+	p.make_move(0x5554);
 	p.make_move(0x2376);
 	p.make_move(0x5455);
 	p.make_move(BKS_CASTLE << 16);//castle!!!!
-	p.make_move(WKS_CASTLE << 16); //forget clearing the path...
-	p.make_move(0x7674); //reset the king.
-	p.make_move(0x0604);
+	p.make_move(0x2010);
+	//this should become the en passant pawn for one turn
+	p.make_move( DOUBLE_ADVANCE << 16 | 0x4060);
+	p.make_move(0x3020);//derp move
+	p.make_move(0x3040);
+	//castle tests by clearing out all other pieces
+	p.make_move(0x2606);
+	p.make_move(0x7476);//reset black king
+	p.make_move(0x2505);
+	p.make_move(0x6373);
+	p.make_move(0x1403);
+	p.make_move(0x5572);
+	p.make_move(WKS_CASTLE << 16);
+	p.make_move(0x5171);
+	p.make_move(0x0406);//reset white king
 	p.make_move(BQS_CASTLE << 16);
+	p.make_move(0x2202);
+	p.make_move(0x5363);//derp move...
+	p.make_move(0x2101);
+	p.make_move(0x6353);
 	p.make_move(WQS_CASTLE << 16);
-	p.make_move( (EN_PASSANT<<16) + 0x4060);//this should reset by next turn
-	p.make_move(0x3010);//derp move
+	 //set up this pawn for en passant death
+	p.make_move(DOUBLE_ADVANCE << 16 | 0x4767);
+	//not a legal passant capture, but due to the flag, the en passant pawn
+	//should now be captured
+	p.make_move(EN_PASSANT << 16 | 0x4725);
 
+	//switching the en passant pawn
+	p.make_move(DOUBLE_ADVANCE << 16 | 0x4666);
+	p.make_move(DOUBLE_ADVANCE << 16 | 0x3717);
+	p.make_move(EN_PASSANT << 16 | 0x3746);
+
+	//promotions
+	p.make_move( (PROMOTE_OFFSET + ROOK) << 16|0x7112);
+	p.make_move(0x2030); //black random pawn move
+	p.make_move( (PROMOTE_OFFSET + KNIGHT) << 16 | 0x7071);
+	p.make_move(0x1020);
+	p.make_move( (PROMOTE_OFFSET + QUEEN)<<16 | 0x7170);
+	p.make_move((PROMOTE_OFFSET + QUEEN) << 16 | 0x0010);
+	p.make_move( (PROMOTE_OFFSET + BISHOP)<<16 | 0x7071);
+
+	//promotion to queen
+	//p.make_move();
 	return 0;
 }

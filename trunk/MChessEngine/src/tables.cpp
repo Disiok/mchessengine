e037d804f9d793e6a6f901f89368d60be7115909
SporkInnovations@gmail.com <SporkInnovations@gmail.com@91017150-5900-139c-9019-248a9390ac6b>
2012-08-04 22:12:25 +0000

    Tons of stuff, needs to be tested though.

---
+++ trunk/MChessEngine/src/tables.cpp
@@ -0,0 +1,16 @@
+/*
+ * tables.h
+ * ============================================
+ * (c) Spark Team, Aug 2012.
+ * The Spark Team reserves all intellectual rights to the following source code.
+ * The code may not be distributed or modified for personal use, except with the
+ * express permission of a team member.
+ * ============================================
+ * Contains all the essential declarations for use in the Myriad engine.
+ */
+
+#include "myriad.h";
+
+namespace myriad{
+
+}
